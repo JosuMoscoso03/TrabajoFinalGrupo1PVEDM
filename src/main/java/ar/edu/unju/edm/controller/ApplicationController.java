0f34bce6a2bc31b60ac8a54d72ab1accf4cbf73d
Julieta Mamani <Julieta Mamani@LAPTOP-9IA3U065>
2020-07-05 20:09:56 -0300

    apliationcontroller

--- src/main/java/ar/edu/unju/edm/controller/ApplicationController.java
+++ src/main/java/ar/edu/unju/edm/controller/ApplicationController.java
@@ -1,5 +1,6 @@
 package ar.edu.unju.edm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -13,6 +14,7 @@ import ar.edu.unju.edm.service.IUsuarioService;
 
 @Controller
 public class ApplicationController {
+	@Autowired
 	IUsuarioService usuarioService;
 	public ApplicationController() {
 		super();
